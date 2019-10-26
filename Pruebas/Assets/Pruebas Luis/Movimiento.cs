1169d305b03e1cfb145bd8af37f3f39d0abfc357
4siul4 <luigis1000@gmail.com>
2019-10-26 23:15:33 +0200

    se gira y todo

--- Pruebas/Assets/Pruebas Luis/Movimiento.cs
+++ Pruebas/Assets/Pruebas Luis/Movimiento.cs
@@ -6,7 +6,9 @@ public class Movimiento : MonoBehaviour
 {
    public Rigidbody2D rb;
    public float FuerzaSalto;
+   public float velocidad;
    bool salto;
+   
 
 
     void Start()
@@ -15,19 +17,34 @@ public class Movimiento : MonoBehaviour
        salto = false;
     }
 
+   void Update()
+   {
 
-    void Update()
+   }
+
+    void FixedUpdate()
     {
+       /*
          if (Input.GetButton("d")) {
             rb.velocity = new Vector2(10, rb.velocity.y);
          }
          if (Input.GetButton("a")) {
             rb.velocity = new Vector2(-10, rb.velocity.y);
          }
-         if (Input.GetButtonDown("w") && salto ) {
-            rb.AddForce(new Vector3(0, 2, 0) * FuerzaSalto);
-         }
          
+         */
+         rb.velocity=new Vector2((velocidad * Input.GetAxis("Horizontal")), (rb.velocity.y));
+         
+         if(Input.GetAxis("Horizontal")<0){
+            GirarseIzquierda();
+         }
+         if(Input.GetAxis("Horizontal")>0){
+            GirarseDerecha();
+         }
+
+         if (Input.GetButtonDown("w") && salto ) {
+            rb.AddForce(new Vector3(0, 1, 0) * FuerzaSalto);
+         }
     }
    void OnCollisionExit2D(Collision2D other){
          if (other.gameObject.name == "buen suelo") {
@@ -42,6 +59,12 @@ public class Movimiento : MonoBehaviour
             salto = true;
  
          }
+   }
+   void GirarseIzquierda (){
+      this.gameObject.transform.localScale = new Vector3(-1,1,1);
+   }
+   void GirarseDerecha(){
+      this.gameObject.transform.localScale = new Vector3(1,1,1);
    }
 
 }
